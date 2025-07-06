f6358fcfd213464d4be9a8266c7b7dfeea3a777e
Brian Hill <brian@lingerhere.org>
2025-07-06 11:02:17 -0700

    Got as far as p. 23 of Chapter 2 of Raschka.

--- raschka/rk_ch02-working_with_text.py
+++ raschka/rk_ch02-working_with_text.py
@@ -1,4 +1,7 @@
 import os
+import re
+
+# Raschka p. 22
 
 LLMS_FROM_SCRATCH_BASE = "../../llms-from-scratch"
 THE_VERDICT_FILENAME = "ch02/01_main-chapter-code/the-verdict.txt"
@@ -9,3 +12,9 @@ with open(the_verdict_full_path, "r", encoding="utf-8") as f:
     raw_text = f.read()
     print("Total number of character:", len(raw_text))
     print(raw_text[:99])
+
+text = "Hello, world. This, is a test."
+result = re.split(r'(\s)', text)
+print(result)
+
+# Raschka p. 23
