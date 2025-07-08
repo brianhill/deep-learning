902330a0e977962744e502103730160d903a6d02
Brian Hill <brian@lingerhere.org>
2025-07-08 10:16:43 -0700

    Got as far as p. 44 of Chapter 2 of Raschka.

--- raschka/rk_ch02-working_with_text.py
+++ raschka/rk_ch02-working_with_text.py
@@ -1,8 +1,10 @@
 import os
 import re
-from importlib.metadata import version
 import tiktoken
-print("tiktoken version:", version("tiktoken"))
+# from importlib.metadata import version
+# print("tiktoken version:", version("tiktoken"))  # 0.9.0
+import torch
+from torch.utils.data import Dataset, DataLoader
 
 # Raschka p. 22
 
@@ -132,4 +134,86 @@ print(tik_test_integers)
 tik_test_strings = bpe_tokenizer.decode(tik_test_integers)
 print(tik_test_strings)
 
-# NEED TO READ p. 34 BEFORE GOING ON TO p. 35 (THE END OF THE CHAPTER IS p. 49)
+# Raschka p. 35
+
+enc_text = bpe_tokenizer.encode(raw_text)
+
+print(len(enc_text))
+
+enc_sample = enc_text[50:]
+
+context_size = 4
+x = enc_sample[:context_size]
+y = enc_sample[1:context_size + 1]
+
+print(f"x: {x}")
+print(f"y:      {y}")
+
+# for i in range(1, context_size + 1):
+#     context = enc_sample[:i]
+#     desired = enc_sample[i]
+#     print(context, "---->", desired)
+#     print(bpe_tokenizer.decode(context), "---->", bpe_tokenizer.decode([desired]))
+
+# Raschka p. 37
+
+class GPTDatasetV1(Dataset):
+    def __init__(self, txt, tokenizer, max_length, stride):
+        self.input_ids = []
+        self.target_ids = []
+        token_ids = tokenizer.encode(txt)
+        for i in range(0, len(token_ids) - max_length, stride):
+            input_chunk = token_ids[i: i + max_length]
+            target_chunk = token_ids[i + 1: i + max_length + 1]
+            self.input_ids.append(torch.tensor(input_chunk))
+            self.target_ids.append(torch.tensor(target_chunk))
+
+    def __len__(self):
+        return len(self.input_ids)
+
+    def __getitem__(self, idx):
+        return self.input_ids[idx], self.target_ids[idx]
+
+
+def create_dataloader_v1(txt, batch_size=4, max_length=256,
+                         stride=128, shuffle=True, drop_last=True,
+                         num_workers=0):
+    tokenizer = tiktoken.get_encoding("gpt2")
+    dataset = GPTDatasetV1(txt, tokenizer, max_length, stride)
+    dataloader = DataLoader(dataset,
+                            batch_size=batch_size,
+                            shuffle=shuffle,
+                            drop_last=drop_last,
+                            num_workers=num_workers)
+    return dataloader
+
+dataloader_v1_1 = create_dataloader_v1(
+    raw_text,
+    batch_size=1,
+    max_length=4,
+    stride=1,
+    shuffle=False
+)
+
+data_iter = iter(dataloader_v1_1)
+first_batch = next(data_iter)
+print(first_batch)
+
+second_batch = next(data_iter)
+print(second_batch)
+
+dataloader_v1_2 = create_dataloader_v1(
+    raw_text,
+    batch_size=8,
+    max_length=4,
+    stride=4,
+    shuffle=False
+)
+
+data_iter = iter(dataloader_v1_2)
+inputs, targets = next(data_iter)
+
+print("Inputs:\n", inputs)
+print("\nTargets:\n", targets)
+
+# Raschka p. 42
